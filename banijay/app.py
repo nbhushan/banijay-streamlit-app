c7d8949d295e0f7d98d3f0765b0b9b9873f752f4
Nitin Bhushan <bhushan.nitin@posteo.net>
2023-03-15 21:31:59 +0000

    added streamlit confog toml

--- banijay/app.py
+++ banijay/app.py
@@ -26,10 +26,6 @@ This is an app that is dynamically updated when new data is available.
 #read in the dataframe
 df_merged = pd.read_csv("/workspaces/banijay-streamlit-app/banijay/data/banijay_merged.csv", infer_datetime_format=True)
 
-'''
-## Monthly Trend Analysis
-'''
-
 #visualize monthly trends
 monthly_trend = (df_merged
     .pipe(to_datetime, ['date_time'], '%Y-%m-%d %H:%M:%S')
@@ -40,8 +36,6 @@ monthly_trend = (df_merged
     .mean()
 )
 
-st.area_chart(monthly_trend)
-
 #target group analysis
 df_target = (df_merged
     .loc[df_merged['ratings type'] == 'totaal', :]
@@ -51,7 +45,11 @@ df_target = (df_merged
 )
 
 '''
+## Monthly Trend Analysis
+'''
+st.area_chart(monthly_trend)
+
+'''
 ## Target Group Analysis
 '''
-
 st.bar_chart(df_target)
